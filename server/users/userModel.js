050b5a2e70edd1ff4a9ee50be098a343123af33a
Jonathan Ng <jon7ng@gmail.com>
2014-12-21 11:54:13 -0800

    (fix) fix spacing

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,22 +1,22 @@
 var mongoose = require('mongoose')
-	bcrypt   = require('bcrypt-nodejs'),
-	Schema   = mongoose.Schema;
+    bcrypt   = require('bcrypt-nodejs'),
+    Schema   = mongoose.Schema;
 
 // define user schema
 var userSchema = new Schema({
-	email: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	firstName: String,
-	lastName: String,
-	status: String,
-	skills: [],
-	interests: String
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: String,
+  lastName: String,
+  status: String,
+  skills: [],
+  interests: String
 });
 
 // checks if the user-input password is correct, returns a boolean
@@ -26,9 +26,9 @@ userSchema.methods.verifyPassword = function(candidatePassword) {
 
 // generates and saves a hashed password for new users
 userSchema.pre('save', function(next) {
-	var hash = bcrypt.hashSync(this.password);
-	this.password = hash;
-	next();
+  var hash = bcrypt.hashSync(this.password);
+  this.password = hash;
+  next();
 });
 
 // compile user schema into a user model
